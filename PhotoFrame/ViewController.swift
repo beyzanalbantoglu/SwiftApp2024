978ea81d9b349760f060ca6439a99805057a741f
beyzanalbantoglu <beyzanalbantoglu00@gmail.com>
2024-09-05 22:51:19 +0300

    Initial Commit

---
+++ PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Beyza NALBANTOĞLU on 3.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
